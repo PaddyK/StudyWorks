65d00146fb1ef7daa66e5cda87b311b8a5b6b040
Patrick Kalmbach <patrick_kalmbach@web.de>
2014-01-20 04:48:53 +0100

    Functionality for reading some raw data from database and writing it fo file added

--- data/DataController.java
+++ data/DataController.java
@@ -1,5 +1,8 @@
 package data;
+import java.util.ArrayList;
+import java.util.List;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.FilenameFilter;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
@@ -7,7 +10,12 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.PreparedStatement;
 
 /**
  * This class supplies interfaces to work with data needed with respect to the conducted Study works.
@@ -18,6 +26,47 @@ import java.sql.*;
  *
  */
 public class DataController {
+	/**
+	 * Connection String for mysql database
+	 */
+	private String jdbcConnectionString;
+	/**
+	 * Name of mysql database user
+	 */
+	private String user;
+	/**
+	 * Password for database user
+	 */
+	private String password;
+	/**
+	 * Url to connect to mysql database
+	 */
+	private String databaseUrl;
+	/**
+	 * Connection to mysql database. A global variable so it is instanciated only once. Very expensive. 
+	 */
+	Connection databaseConnection;
+	
+	/**
+	 * Constructor for class DataController. Takes a username and password.
+	 * @param username	username for mysql database
+	 * @param password	password for user specified with username for mysql databse
+	 */	
+	public DataController(String username, String password) {
+		this.user = username;
+		this.password = password;
+		this.jdbcConnectionString = "com.mysql.jdbc.Driver";
+		this.databaseUrl = "jdbc:mysql://localhost/StudyWorks";
+		
+		try {
+			this.databaseConnection = DriverManager.getConnection(this.databaseUrl, this.user, this.password);
+		}
+		catch(SQLException e) {
+			System.err.println("Failed to create connection to database: url " + this.databaseUrl +
+					"user " + this.user);
+			e.printStackTrace();
+		}
+	}
 
 	/**
 	 * Reads all .gpr files from a directory into mysql database and includes identifier from sample name
@@ -115,6 +164,110 @@ public class DataController {
 		}
 		catch(IOException e) {
 			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Reads data for specified antibodies from each patient from raw data.
+	 * @param antibodyIds	List of ids from antibodies as specified in table ArrayDesign for which information should
+	 * 						be retrieved from table Rawdata
+	 * @return				List of RawDataMinimal elements
+	 */
+	public List<RawDataMini> readMinimalRawData(java.util.List<Integer> antibodyIds) {
+		ResultSet data;
+		RawDataMini tmp;
+		List<RawDataMini> rawData = null;
+		PreparedStatement statement = null;
+		String stmnt = "SELECT Patient_id" +
+				",sdr.Sample_source_name" +
+				",rd.F635_Median" +
+				",rd.F635_Mean" +
+				",F635_SD" +
+				",rd.B635" +
+				",rd.B635_Median" +
+				",rd.B635_Mean" +
+				",rd.B635_SD" +
+				",rd.F532_Median" +
+				",rd.F532_Mean" +
+				",rd.F532_SD " +
+				",rd.B532" +
+				",rd.B532_Median" +
+				",rd.B532_Mean" +
+				",rd.B532_SD FROM RawData AS rd INNER JOIN SamleAndDataRelationship as sdr " +
+				"	ON rd.Patient_id LIKE CONCAT(sdr.PatientId, '%')" +
+				"WHERE rd.ID IN (SELECT ad.DatabaseId FROM ArrayDesign WHERE idArrayDesign IN (";
+		
+		for(int i : antibodyIds)
+			stmnt += "?,";
+		// Remove trailing comma
+		stmnt = stmnt.substring(0, stmnt.length() - 2) + "));";
+		
+		try {
+			statement = (PreparedStatement)this.databaseConnection.prepareStatement(stmnt);
+			for(int i = 0; i < antibodyIds.size(); i++)
+				statement.setInt(i, antibodyIds.get(i));
+			data = statement.executeQuery();
+			rawData = new ArrayList<RawDataMini>();
+			while(data.next()) {
+				tmp = new RawDataMini();
+				tmp.setPatientId(data.getString("Patient_id"));
+				tmp.setHasParkinson((data.getString("").toLowerCase().contains("pd"))? "yes" : "no");
+				tmp.setF635median(data.getInt("F635_Median"));
+				tmp.setF635mean(data.getInt("F635_Mean"));
+				tmp.setF635sd(data.getInt("F635_SD"));
+				tmp.setB635(data.getInt("B635"));
+				tmp.setB635median(data.getInt("B635_Median"));
+				tmp.setB635mean(data.getInt("B635_Mean"));
+				tmp.setB635sd(data.getInt("B635_SD"));
+				tmp.setF532median(data.getInt("F532_Median"));
+				tmp.setF532median(data.getInt("F532_Mean"));
+				tmp.setF532median(data.getInt("F532_SD"));
+				tmp.setB532(data.getInt("B532"));
+				tmp.setB532median(data.getInt("B532_Median"));
+				tmp.setB532mean(data.getInt("B532_Mean"));
+				tmp.setB532sd(data.getInt("B532_SD"));
+				
+				rawData.add(tmp);
+			}
+		} 
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				statement.close();
+			}
+			catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return rawData;		
+	}
+	
+	/**
+	 * Writes the data of a given set of raw data items to a csv file
+	 * @param destination	Path to destination file
+	 * @param data			Set of raw data lines from database
+	 */
+	public void writeRawDataMiniToFile(String destination, List<RawDataMini> data) {
+		File dest = new File(destination);
+		FileWriter writer = null;
+		
+		try {
+			writer = new FileWriter(dest.getAbsolutePath());
+			for(RawDataMini d : data)
+				writer.append(d.toString() + System.getProperty("line.separator"));
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				writer.close();
+			}
+			catch(IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
--- data/RawDataMini.java
+++ data/RawDataMini.java
@@ -1,5 +1,177 @@
 package data;
 
 public class RawDataMini {
-
+	/**
+	 * Patient Id, identifies patient
+	 */
+	protected String patientId;
+	/**
+	 * Nominal feature, yes --> has parkinson, no --> no parkinson
+	 */
+	protected String hasParkinson;
+	/**
+	 * Median of pixel intensities for wavelength of 635nm
+	 */	
+	protected double f635median;
+	/**
+	 * Mean of pixel intensities for wavelength of 635nm
+	 */
+	protected double f635mean;
+	/**
+	 * Standard deviation of pixel intensities for wavelength of 635nm
+	 */
+	protected double f635sd;
+	/**
+	 * Background fluoroscence value of pixel intensities for wavelength of 635nm
+	 */
+	protected double b635;
+	/**
+	 * Background fluoroscence median of pixel intensities for wavelength of 635nm
+	 */
+	protected double b635median;
+	/**
+	 * Background fluoroscence mean of pixel intensities for wavelength of 635nm
+	 */
+	protected double b635mean;
+	/**
+	 * Background fluoroscence standard deviation of pixel intensities for wavelength of 635nm
+	 */
+	protected double b635sd;
+	/**
+	 * Median of pixel intensities for wavelength of 635nm
+	 */
+	protected double f532median;
+	/**
+	 * Mean of pixel intensities for wavelength of 635nm
+	 */
+	protected double f532mean;
+	/**
+	 * Standard Deviation of pixel intensities for wavelength of 635nm
+	 */
+	protected double f532sd;
+	/**
+	 * Background fluoroscence value of pixel intensities for wavelength of 635nm
+	 */
+	protected double b532;
+	/**
+	 * Background fluoroscence median of pixel intensities for wavelength of 635nm
+	 */
+	protected double b532median;
+	/**
+	 * Background fluoroscence mean of pixel intensities for wavelength of 635nm
+	 */
+	protected double b532mean;
+	/**
+	 * Background fluoroscence standard deviation of pixel intensities for wavelength of 635nm
+	 */
+	protected double b532sd;
+	
+	public String getPatientId() {
+		return patientId;
+	}
+	public void setPatientId(String patientId) {
+		this.patientId = patientId;
+	}
+	
+	public String getHasParkinson() {
+		return hasParkinson;
+	}
+	public void setHasParkinson(String hasParkinson) {
+		this.hasParkinson = hasParkinson;
+	}
+	public double getF635median() {
+		return f635median;
+	}
+	public void setF635median(double f635median) {
+		this.f635median = f635median;
+	}
+	public double getF635mean() {
+		return f635mean;
+	}
+	public void setF635mean(double f635mean) {
+		this.f635mean = f635mean;
+	}
+	public double getF635sd() {
+		return f635sd;
+	}
+	public void setF635sd(double f635sd) {
+		this.f635sd = f635sd;
+	}
+	public double getB635() {
+		return b635;
+	}
+	public void setB635(double b635) {
+		this.b635 = b635;
+	}
+	public double getB635median() {
+		return b635median;
+	}
+	public void setB635median(double b635median) {
+		this.b635median = b635median;
+	}
+	public double getB635mean() {
+		return b635mean;
+	}
+	public void setB635mean(double b635mean) {
+		this.b635mean = b635mean;
+	}
+	public double getB635sd() {
+		return b635sd;
+	}
+	public void setB635sd(double b635sd) {
+		this.b635sd = b635sd;
+	}
+	public double getF532median() {
+		return f532median;
+	}
+	public void setF532median(double f532median) {
+		this.f532median = f532median;
+	}
+	public double getF532mean() {
+		return f532mean;
+	}
+	public void setF532mean(double f532mean) {
+		this.f532mean = f532mean;
+	}
+	public double getF532sd() {
+		return f532sd;
+	}
+	public void setF532sd(double f532sd) {
+		this.f532sd = f532sd;
+	}
+	public double getB532() {
+		return b532;
+	}
+	public void setB532(double b532) {
+		this.b532 = b532;
+	}
+	public double getB532median() {
+		return b532median;
+	}
+	public void setB532median(double b532median) {
+		this.b532median = b532median;
+	}
+	public double getB532mean() {
+		return b532mean;
+	}
+	public void setB532mean(double b532mean) {
+		this.b532mean = b532mean;
+	}
+	public double getB532sd() {
+		return b532sd;
+	}
+	public void setB532sd(double b532sd) {
+		this.b532sd = b532sd;
+	}
+	
+	/**
+	 * Returns properties of object as tab separated string
+	 * @return List of attributes separated by tabulator
+	 */
+	@Override
+	public String toString() {
+		return patientId + "\t" + f635mean + "\t" + f635median + "\t" + f635sd + "\t" + b635 + "\t" + b635mean + "\t" +
+				b635median + "\t" + b635sd + "\t" + f532mean + "\t" + f532median + "\t" + f532sd + "\t" + b532 + "\t" +
+				b532mean + "\t" + b532median + "\t" + b532sd + "\t" + hasParkinson;
+	}
 }
